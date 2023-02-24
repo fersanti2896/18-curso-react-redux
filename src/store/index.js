082a31a5ba4714268677b8738ae8669e7aaf2f37
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-24 15:32:43 -0600

    RTK Query en todosAPI.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
 
+export * from './apis';
 export * from './slice';
 export * from './store';
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { todosAPI } from './apis';
 import { counterSlice, pokemonSlice } from './slice';
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
-        pokemons: pokemonSlice.reducer
-    }
+        pokemons: pokemonSlice.reducer,
+
+        [todosAPI.reducerPath]: todosAPI.reducer,
+    },
+    /* Middleware */
+    middleware: ( getDefaultMiddleware ) => getDefaultMiddleware().concat( todosAPI.middleware )
 })
